582ca1482f6bbbff4a63274f6c4dbe1842a4b262
sancli <sancli@cs.wallawalla.edu>
2024-03-10 14:18:31 -0700

    autosave

--- src/Jenessy/Jen_Color_Test.cpp
+++ src/Jenessy/Jen_Color_Test.cpp
@@ -1,16 +1,14 @@
-#include <iostream> 
-#include <stdlib.h> 
 #include "color_function.cpp"
-using namespace std; 
-  
-// Driver Code 
-int main() 
-{ 
-    color_text("Hello", 'R');
-    color_text("Hello", 'Y');
-    color_text("Hello", 'G');
-    color_text("Hello", 'B');
-    
-    return 0; 
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
 
+// Driver Code
+int main() {
+  color_text("Hello", 'R');
+  color_text("Hello", 'Y');
+  color_text("Hello", 'G');
+  color_text("Hello", 'B');
+
+  return 0;
 }
--- src/Jenessy/color_function.cpp
+++ src/Jenessy/color_function.cpp
@@ -1,6 +1,6 @@
 #include "colors.h"
-#include <string>
 #include <iostream>
+#include <string>
 
 #define R COLOR_RED
 #define Y COLOR_YELLOW
@@ -8,36 +8,37 @@
 #define B COLOR_BLUE
 #define W COLOR_WHITE
 
-//takes text and desired color (R,Y,G,B) as arguments and outputs colored text (Input text will have an endl attached once printed)
+// takes text and desired color (R,Y,G,B) as arguments and outputs colored text
+// (Input text will have an endl attached once printed)
 void color_text(std::string my_text, char my_color) {
-    std::string colored_string, colored_set;
+  std::string colored_string, colored_set;
 
-    switch(my_color) {
-        case 'R':
-            colored_set = R; // Sets text to red
-            std::cout << colored_set;
-            std::cout << my_text << std::endl;
-            break;
-        
-        case 'Y':
-            colored_set = Y; // Sets text to yellow
-            std::cout << colored_set;
-            std::cout << my_text << std::endl;
-            break;
-        
-        case 'G':
-            colored_set = G; // Sets text to green
-            std::cout << colored_set;
-            std::cout << my_text << std::endl;
-            break;
-        
-        case 'B':
-            colored_set = B; // Sets text to blue
-            std::cout << colored_set;
-            std::cout << my_text << std::endl;
-            break;
-        }
+  switch (my_color) {
+  case 'R':
+    colored_set = R; // Sets text to red
+    std::cout << colored_set;
+    std::cout << my_text << std::endl;
+    break;
 
-    colored_string = W;
-    std::cout << colored_string; 
+  case 'Y':
+    colored_set = Y; // Sets text to yellow
+    std::cout << colored_set;
+    std::cout << my_text << std::endl;
+    break;
+
+  case 'G':
+    colored_set = G; // Sets text to green
+    std::cout << colored_set;
+    std::cout << my_text << std::endl;
+    break;
+
+  case 'B':
+    colored_set = B; // Sets text to blue
+    std::cout << colored_set;
+    std::cout << my_text << std::endl;
+    break;
+  }
+
+  colored_string = W;
+  std::cout << colored_string;
 }
