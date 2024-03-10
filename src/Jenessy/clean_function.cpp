2b3731d3387289a89105042c4474ca9da2e53523
lustje <lustje@cs.wallawalla.edu>
2024-03-10 14:06:23 -0700

    autosave

---
+++ src/Jenessy/clean_function.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <iostream>
+
+void clean_action(std::string pet_name) {
+    std::cout << "It is bathtime!" << std::endl;
+    //show sprite of bathtime
+
+    // random chance scenario >> increase affection or decrease health
+    // 3 random chance scenarios using random
+       // Your pet was extra relaxed and played with bubbles.
+       // Your pet feels pampered and clean.
+       // your pet broke its ankle getting out of the bath.
+       // Your pet got soap in its eyes. Ouch!     
+
+    //lower daily counter
+
+    //increase clean bar
+
+    //increase affection
+
+}
