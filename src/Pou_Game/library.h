980091f5a78757bea833f5efbbabf8d8476db97f
lustje <lustje@cs.wallawalla.edu>
2024-03-14 16:18:57 -0700

    autosave

--- src/Pou_Game/library.h
+++ src/Pou_Game/library.h
@@ -5,4 +5,52 @@
 
 #include <string>
 
-std::string helloWorld();
+class Bars {
+public:
+  // getter functions for each bar
+  int getHealth();
+  int getHappiness();
+  int getHunger();
+  int getSleep();
+  int getHygiene();
+
+  // allows bars to be set to a different number
+  int setHealth(int);
+  int setHappiness(int);
+  int setHunger(int);
+  int setSleep(int);
+  int setHygiene(int);
+
+  // info about all the bars and functions that affect all bars
+  void barStatus();
+  void decreasePoints(int amount = 10);
+  void increasePoints(int amount = 5);
+
+  void decreaseHealthPoints(int amount = 10);
+  void increaseHealthPoints(int amount = 5);
+
+  void decreaseHappinessPoints(int amount = 10);
+  void increaseHappinessPoints(int amount = 5);
+
+  void decreaseHungerPoints(int amount = 10);
+  void increaseHungerPoints(int amount = 5);
+
+  void decreaseSleepPoints(int amount = 10);
+  void increaseSleepPoints(int amount = 5);
+
+  void decreaseHygienePoints(int amount = 10);
+  void increaseHygienePoints(int amount = 5);
+
+  // initializes the bars to the starting amount of points
+  Bars(int health = 100, int happiness = 40, int hunger = 100, int sleep = 100,
+       int hygiene = 100);
+
+private:
+  const int MAX = 100;
+  int value;
+  int health;
+  int happiness;
+  int hunger;
+  int sleep;
+  int hygiene;
+};
--- src/Pou_Game/main.cpp
+++ src/Pou_Game/main.cpp
@@ -1,10 +1,56 @@
 /**
  * Project III: Start here.
  */
-#include "library.h"
+
 #include "library.cpp"
 #include <iostream>
+#include <string>
 
 int main() {
+  // Print the menu for pet selection
+  char petChoice;
+  std::string petName;
 
+  do {
+    std::cout
+              << "======================================\n          ❥ Welcome "
+                 "to My Pet! ❥\n======================================\n"
+               << std::endl;
+
+    std::cout << "Choose your pet:" << std::endl
+              << "a) Dog" << std::endl
+              << "b) Cat" << std::endl
+              << "c) Frog\n"
+              << std::endl
+              << "Pet Choice: ";
+
+    std::cin >> petChoice;
+    petChoice = tolower(petChoice);
+
+    switch (petChoice) {
+    case 'a':
+      std::cout << "Enter the name of your pet: ";
+      std::cin >> petName;
+      // Bars dogBar;
+      // dogBar.barStatus();
+      // create obj
+      break;
+
+    case 'b':
+      std::cout << "Enter the name of your pet: ";
+      std::cin >> petName;
+      break;
+
+    case 'c':
+      std::cout << "Enter the name of your pet: ";
+      std::cin >> petName;
+      break;
+    }
+
+  } while ((petChoice == 'a') || (petChoice == 'b') || (petChoice == 'c'));
+
+  std::cout << COLOR_RED
+            << "INVALID ENTRY: VALID INPUTS INCLUDE \"a\", \"b\", or \"c\""
+            << std::endl;
+  std::cin >> petChoice;
 }
